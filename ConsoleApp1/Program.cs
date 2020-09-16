23232efc62f39e762a72946efdff5d5d303b53b5
Justin  Ramjattan <justin.ramjattan@diagonconsulting.com>
2020-09-16 12:36:48 -0400

    Added handwriting code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Google.Cloud.Vision.V1;
+using RestSharp;
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            //                     GOOGLE CLOUD API
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "<location of key.json>");
+            var image = Image.FromFile("<location of image file>");
+            var client = ImageAnnotatorClient.Create();
+            var response = client.DetectDocumentText(image);
+            Console.WriteLine(response.Text);
+
+
+        }
+    }
+}
